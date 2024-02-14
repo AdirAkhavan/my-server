b97fb672c73e9e9b5811e610513437f04d82b9b5
AdirAkhavan <adirak@evolution.inc>
2024-02-14 09:01:45 +0200

    root enforcement updated

--- src/RequestHandler.java
+++ src/RequestHandler.java
@@ -8,7 +8,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Map;
-import java.util.Stack;
 
 class RequestHandler implements Runnable {
     private Socket clientSocket;
@@ -31,20 +30,15 @@ class RequestHandler implements Runnable {
             printlnWithColor("HTTP/1.1 500 Internal Server Error\r\n\r\n");
         }
     }
-
-    public static String simplifyPath(String path) {
+    
+    public static String enforceRoot(String path) {
         String[] components = path.split("/");
-        Stack<String> stack = new Stack<>();
-        
+        StringBuilder simplifiedPath = new StringBuilder();
+
         for (String component : components) {
             if (!component.equals("..") && !component.isEmpty()) {
-                stack.push(component);
+                simplifiedPath.append("/").append(component);
             }
-        }
-
-        StringBuilder simplifiedPath = new StringBuilder();
-        for (String dir : stack) {
-            simplifiedPath.append("/").append(dir);
         }
         
         if (simplifiedPath.length() == 0){
@@ -170,7 +164,7 @@ class RequestHandler implements Runnable {
         String responseToPrint = "";
         String response = "No response";
 
-        requestedResource = simplifyPath(requestedResource);
+        requestedResource = enforceRoot(requestedResource);
 
         // Handle request for the HTML file
         if (requestedResource.equals("/") || requestedResource.equals("/" + defaultPage)) {
@@ -239,9 +233,8 @@ class RequestHandler implements Runnable {
         else {
             // handling any other file
             String filePath = rootDirectory + requestedResource;
-            String simpleFilePath = simplifyPath(filePath);
 
-            File file = new File(simpleFilePath);
+            File file = new File(filePath);
     
             if (file.exists() && !file.isDirectory()) {
                 byte[] content = readFileAsBytes(file);
@@ -297,6 +290,8 @@ class RequestHandler implements Runnable {
         String requestedResource = httpRequest.requestedPage;
         String responseToPrint = "";
         String response = "No response";
+
+        requestedResource = enforceRoot(requestedResource);
 
         if (requestedResource.equals("/") || requestedResource.equals("/" + defaultPage)) {
             String filePath = rootDirectory + defaultPage;
